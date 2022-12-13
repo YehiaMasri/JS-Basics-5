3582925927ed7e0ebe9674a9bb6953de01e279f9
Abdellatif <boudikerde1@gmail.com>
2022-12-13 16:37:15 +0200

    problem2

---
+++ problem2.js
@@ -0,0 +1,12 @@
+var x = document.getElementById("h")
+const inputField = document.getElementById("name"); 
+x.onclick = ()=> {
+    let name = inputField.value;
+    if(name=="bob"){
+    alert("Hello " + name);
+    inputField.value = "";
+}else if (name=="alice"){
+  alert("Hello " + name);
+  inputField.value = "";
+}
+}
